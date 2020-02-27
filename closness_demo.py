cf76b33f3c8290ea07c3927a36671331fe20272d
rohanchandra30 <rohanchandra30@gmail.com>
2020-02-27 12:50:22 -0500

    adding more automation

--- closness_demo.py
+++ closness_demo.py
@@ -5,8 +5,11 @@ import numpy as np
 from adjacency import *
 
 # SET THESE TWO PARAMS
-num = 6 # See "sets" variable
-cen_num = 1 #0:closeness, 1:degree, 2:eigenvector
+num = 21 # [0-6] See "sets" variable. Set 2 has 3 agents, so you have to give options '20', '21', and '22' which correspond to the 1st, 2nd, and 3rd agents respectively
+if num >= 20:
+    agent_num = num%20
+    num = int(num/10)
+cen_num = 1 #0:closeness (num == 0,20,21,22), 1:degree (num == 1 and 6), 2:eigenvector (num == 3,4)
 
 
 centrality_labels = ['Closeness Centrality Value','Degree Centrality Value','Eigenvector Centrality Value']
@@ -20,14 +23,14 @@ sets = ['4',        #0
         '13-2019-08-27-22-30-18', #5
         '16_30_18', #6
         '10']       #7
-frames= [[0,60],[47,80],[55,95], [52,98],[14,50], [59,85], [65,72]]
+frames= [[0,60],[47,80],[[55,95],[70,98]], [52,98],[14,50], [59,85], [65,72]]
 agent_IDs = [983,2677,[2810,2958,2959],1336,3494,1295,1786]
 radius = [10,20,10,20,20,10,10]
 agent_labels = ['Black Car', 'White Car', 'White Car', 'White Bus', 'White Truck', 'White Lorry', 'Motorbike']
 centrality_label = centrality_labels[cen_num]
 
 set = sets[num]
-frame = frames[num]
+frame = frames[num] if num != 2 else frames[num][agent_num]
 agent_ID = agent_IDs[num]
 rad = radius[num]
 agent_label = agent_labels[num]
@@ -53,7 +56,7 @@ for each_frame in sample_annotation:
 
 to_array = np.asarray(to_list)
 obj_IDs = np.unique(to_array[:, 1]).astype(int)
-agent_idx = list(obj_IDs).index(agent_ID[0]) if num==2 else list(obj_IDs).index(agent_ID)
+agent_idx = list(obj_IDs).index(agent_ID[agent_num]) if num==2 else list(obj_IDs).index(agent_ID)
 adj_mats = generate_adjacency(to_array,rad)
 weave_list = []
 weave_list2 = []
