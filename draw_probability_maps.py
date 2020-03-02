91add22944847695382cdfecdc895dd1d0ab22e0
UttaranB127 <utta005@gmail.com>
2020-03-01 21:26:35 -0500

    Minor changes in draw_probability_maps

--- draw_probability_maps.py
+++ draw_probability_maps.py
@@ -36,19 +36,19 @@ with open('aggressive_frames.csv') as csv_file:
 
 # ================================================================================================
 
-# 0 - '30_18_7(front)',
-# 1 - '30_18_10(rear)',
-# 2 - '30_18_11(front)',
-# 3 - '30_18_11(rear)',
-# 4 - '30_18_13(front)',
-# 5 - '30_18_16(front)',
-# 6 - '47_10_9(front)',
-# 7 - '47_10_9(rear)',
-# 8 - '55_47_4(rear)',
-# 9 - '55_47_5(rear)',
-# 10 - '55_47_12(front)'
+# 0 - '30_18_7(front)' -    SLC         77.0    79  2,
+# 1 - '30_18_10(rear)' -    SLC         28.125  31  2.875,
+# 2 - '30_18_11(rear)' -    W           67.375  69  1.625,
+# 3 - '30_18_11(front)' -   OT          94.8125 96  1.1875,
+# 4 - '30_18_13(front)' -   W           71.5    75  3.5,
+# 5 - '30_18_16(front)' -   OS          70.0625 68  2.0625,
+# 6 - '47_10_9(front)' -    OS          17.875  18  0.125,
+# 7 - '47_10_9(rear)' -     OT          75.0625 75  0.0625,
+# 8 - '55_47_4(rear)' -     OT          45.0    49  4,
+# 9 - '55_47_5(rear)' -     SLC         72.5625 76  3.4375,
+# 10 - '55_47_12(front) -   OS
 
-num = 0
+num = 7
 cen_num = 0
 color = True
 
@@ -72,9 +72,9 @@ if 40 <= num <= 80:
 #           [58, 90],
 #           [10, 98]]
 frames = [[52, 98], [14, 50], [55, 95], [70, 98], [59, 85], [65, 72],
-          [50, 98], [9, 35], [0, 60], [58, 90], [10, 98]]
+          [9, 35], [50, 98], [0, 60], [58, 90], [10, 98]]
 # agent_IDs = [983, 2677, [2810, 2958, 2959], 1336, 3494, 1295, 1786, [[2562], [2564]], 1750, 868]
-agent_IDs = [1336, 3494, [2810, 2958], 2959, 1295, 1786, 2562, 2564, 983, 1750, 2677, 868]
+agent_IDs = [1336, 3494, 2958, 2959, 1295, 1786, 2564, 2562, 983, 1750, 2677, 868]
 radius = [10, 20, 10, 20, 20, 10, 10, 10, 10, 10]
 agent_labels = ['Black Car', 'White Car', 'White Car', 'White Bus',
                 'White Truck', 'White Lorry', 'Motorbike', 'Scooter', 'Scooter', 'Motorbike']
@@ -146,9 +146,11 @@ for fr, item in enumerate(adj_mats):
 
 buffer2 = 7 if len(weave_list2) % 2 == 0 else 8
 buffer = 7 if len(weave_list) % 2 == 0 else 8
-if num != 6:
+if num != 5:
     weave_list2 = signal.savgol_filter(weave_list2, len(weave_list2)-buffer2, 3)
     # weave_list = signal.savgol_filter(weave_list, len(weave_list)-buffer, 3)
+else:
+    weave_list2 = np.asarray(weave_list2)
 
 
 cmaps = ['#0038ff', '#00a4b2', '#4c6fb8', '#2c5167', '#9fd9ea']
@@ -207,4 +209,5 @@ plt.show()
 
 weave_list_grad = np.abs(weave_list2[1:] - weave_list2[:-1])
 estimated_mean_frame = fr_list[np.argmax(weave_list_grad)]
+data_mean_frame = np.mean(x_lims)
 temp = 1
